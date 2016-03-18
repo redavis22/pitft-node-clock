e4a9b98de9f48d9ff2f2facf4eb5ac616d77e726
redavis22 <redavis22@gmail.com>
2016-03-18 19:09:14 -0400

    added .png

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ var yMax = fb.size().height;
 var radius = yMax / 2 - 10;
 //var imageFilePath = __dirname + "/raspberry-pi-icon.png";
 //var imageFilePath = __dirname + "/raj2pic2.png";
-var imageFilePath = __dirname + "/rsz_img_8112.jpg"
+var imageFilePath = __dirname + "/rsz_img_8112.png"
 var RA = 180 / Math.PI;
 
 var drawDial = function() {
